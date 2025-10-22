992e11392f8a926fe8987b7e8a07116b77c08cc7
cl6251 <christian.luna@icloud.com>
2025-10-21 20:14:27 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,11 +11,7 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Hello world <span className="sr-only">916tools</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
+    
             <img
               src="/full_logo_service.png"
               alt="Remix"
