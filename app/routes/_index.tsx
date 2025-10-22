e18b0286df8fad304824332f37b6e103e76b1025
cl6251 <christian.luna@icloud.com>
2025-10-21 20:22:46 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,10 +10,14 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
-      
-    
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Welcome to <span className="sr-only">916tools</span>
+          </h1>
+          <div className="h-[144px] w-[434px]">
             <img
-              src="/full_logo_service.png"
+              src="/logo-light.png"
               alt="Remix"
               className="block w-full dark:hidden"
             />
@@ -23,7 +27,7 @@ export default function Index() {
               className="hidden w-full dark:block"
             />
           </div>
-        
+        </header>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
           <p className="leading-6 text-gray-700 dark:text-gray-200">
             What&apos;s next?
